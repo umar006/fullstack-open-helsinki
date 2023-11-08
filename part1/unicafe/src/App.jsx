3c565fdf4fdd0c27cd6a8ce8722c07d834f373d3
umar006 <umaralfaruq597@gmail.com>
2023-11-08 20:41:51 +0700

    Count gathered neutral feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,18 +2,26 @@ import { useState } from "react";
 
 const App = () => {
   const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
 
   const handleIncrementGood = () => {
     const newGood = good + 1;
     setGood(newGood);
   };
 
+  const handleIncrementNeutral = () => {
+    const newNeutral = neutral + 1;
+    setNeutral(newNeutral);
+  };
+
   return (
     <div>
       <h2>give feedback</h2>
       <button onClick={handleIncrementGood}>good</button>
+      <button onClick={handleIncrementNeutral}>neutral</button>
       <h2>statistics</h2>
       <p>good {good}</p>
+      <p>neutral {neutral}</p>
     </div>
   );
 };
