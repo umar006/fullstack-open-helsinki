c2a3b2f2b8d2b5f51c013677a3a0130e89df6019
umar006 <umaralfaruq597@gmail.com>
2023-11-08 20:38:55 +0700

    Count gathered good feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>code here</div>;
+  const [good, setGood] = useState(0);
+
+  const handleIncrementGood = () => {
+    const newGood = good + 1;
+    setGood(newGood);
+  };
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={handleIncrementGood}>good</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+    </div>
+  );
 };
 
 export default App;
