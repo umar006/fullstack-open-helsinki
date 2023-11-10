c0b038f25ac6645371aeac631edb2c48125c914f
umar006 <umaralfaruq597@gmail.com>
2023-11-10 15:47:53 +0700

    Initial commit for exercise courseinfo part2

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,70 @@
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+const Content = (props) => {
+  const parts = props.parts;
+  return (
+    <>
+      <Part name={parts[0].name} exercise={parts[0].exercises} />
+      <Part name={parts[1].name} exercise={parts[1].exercises} />
+      <Part name={parts[2].name} exercise={parts[2].exercises} />
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.name} {props.exercise}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  const parts = props.parts;
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      </p>
+    </>
+  );
+};
+
+export default App;
