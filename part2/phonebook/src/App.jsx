448877fd74cc71a4f287b7fcd02ff1aa706f7797
umar006 <umaralfaruq597@gmail.com>
2023-11-11 18:33:21 +0700

    Add new name to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,37 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+
+  const addPerson = (e) => {
+    e.preventDefault();
+
+    const newPerson = { id: persons.length + 1, name: newName };
+
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
+  const handlePersonChange = (e) => {
+    setNewName(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <div key={person.id}>{person.name}</div>
+      ))}
     </div>
   );
 };
