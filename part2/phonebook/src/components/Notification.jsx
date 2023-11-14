33eeae1c92d917c3e2da7464be765231138517db
umar006 <umaralfaruq597@gmail.com>
2023-11-14 07:59:03 +0700

    Create separate component for success notification

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,9 +1,17 @@
-const Notification = ({ message }) => {
+const SuccessNotification = ({ message }) => {
   if (message === null) {
     return null;
   }
 
   return <div className="success">{message}</div>;
+};
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return <SuccessNotification message={message} />;
 };
 
 export default Notification;
