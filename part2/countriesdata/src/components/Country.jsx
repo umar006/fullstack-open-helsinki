0db023a4f6e78d7c557ec34ebebb34d197668bb0
umar006 <umaralfaruq597@gmail.com>
2023-11-14 12:26:54 +0700

    Display basic data of the country if result only one country

--- part2/countriesdata/src/components/Country.jsx
+++ part2/countriesdata/src/components/Country.jsx
@@ -1,3 +1,20 @@
+const CountryDetail = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>capital {country.capital}</p>
+      <p>area {country.area}</p>
+      <p>languages:</p>
+      <ul>
+        {Object.keys(country.languages).map((language) => (
+          <li key={language}>{country.languages[language]}</li>
+        ))}
+      </ul>
+      <img src={country.flags.svg} alt={country.flags.alt} />
+    </div>
+  );
+};
+
 const Country = ({ country }) => {
   return <div>{country.name.common}</div>;
 };
@@ -5,11 +22,20 @@ const Country = ({ country }) => {
 const CountryList = ({ countries, query }) => {
   const searchedCountries = countries
     .filter((country) => country.name.common.toLowerCase().includes(query))
-    .map((country) => <Country key={Number(country.ccn3)} country={country} />);
+    .map((country, _, countries) => {
+      if (countries.length === 1)
+        return <CountryDetail key={Number(country.ccn3)} country={country} />;
 
-  if (searchedCountries.length > 10) {
-    return <div>Too many matches, specify another filter</div>;
-  }
+      if (countries.length > 10) {
+        return (
+          <div key={Number(country.ccn3)}>
+            Too many matches, specify another filter
+          </div>
+        );
+      }
+
+      return <Country key={Number(country.ccn3)} country={country} />;
+    });
 
   return searchedCountries;
 };
