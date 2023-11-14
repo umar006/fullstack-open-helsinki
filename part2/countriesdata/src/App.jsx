0c99fe127cac1b6b7663961a26d1d725ae780e42
umar006 <umaralfaruq597@gmail.com>
2023-11-14 11:00:28 +0700

    Get country list with axios

--- part2/countriesdata/src/App.jsx
+++ part2/countriesdata/src/App.jsx
@@ -1,7 +1,18 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useState } from "react";
 
+const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
+
 function App() {
+  const [countries, setCountries] = useState([]);
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    axios.get(baseUrl + "/all").then((res) => {
+      setCountries(res.data);
+    });
+  }, []);
 
   const handleQueryChange = (e) => {
     setQuery(e.target.value);
