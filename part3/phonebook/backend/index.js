ac25cc2f213f9d89aa131ed82d389544c76cb116
umar006 <umaralfaruq597@gmail.com>
2023-11-16 13:46:11 +0700

    Create address that returns phonebook list

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -2,8 +2,31 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+const persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
 });
 
 const PORT = 3001;
