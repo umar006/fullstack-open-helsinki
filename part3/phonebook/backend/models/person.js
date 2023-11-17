f11182b4ce85ee69c0bfe0ccaf724cf74fefc27b
umar006 <umaralfaruq597@gmail.com>
2023-11-17 14:08:53 +0700

    Create Person model

---
+++ part3/phonebook/backend/models/person.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const uri = process.env.MONGODB_URI;
+
+mongoose
+  .connect(uri)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+export default Person;
