88b250d55ceb7a21c035fe49f5a7d7a7f77182f5
umar006 <umaralfaruq597@gmail.com>
2023-11-18 19:55:24 +0700

    Create error handler middleware

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -91,6 +91,18 @@ app.get("/info", (request, response) => {
   response.send(htmlInfo);
 });
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
