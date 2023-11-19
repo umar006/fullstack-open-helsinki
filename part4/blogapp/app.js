470ca45dbc367a8e53dfe18543aecbf88d120531
umar006 <umaralfaruq597@gmail.com>
2023-11-20 06:43:13 +0700

    Separate express app to app file

---
+++ part4/blogapp/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
+const blogRouter = require("./controllers/blogs");
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
--- part4/blogapp/index.js
+++ part4/blogapp/index.js
@@ -1,20 +1,6 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+const app = require("./app");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-require("dotenv").config();
-
-const blogRouter = require("./controllers/blogs");
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl).then(() => console.log("connect"));
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/blogs", blogRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
