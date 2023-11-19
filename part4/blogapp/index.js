ba1d46874b1186397fce2f6f7c9ea51961407633
umar006 <umaralfaruq597@gmail.com>
2023-11-20 06:39:03 +0700

    Move route handler into a dedicated module

--- part4/blogapp/index.js
+++ part4/blogapp/index.js
@@ -6,27 +6,15 @@ const config = require("./utils/config");
 const logger = require("./utils/logger");
 require("dotenv").config();
 
-const Blog = require("./models/blog");
+const blogRouter = require("./controllers/blogs");
 
 const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl);
+mongoose.connect(mongoUrl).then(() => console.log("connect"));
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
