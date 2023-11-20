ac0005065d0816f3341552f8f2ef1bde84d49708
umar006 <umaralfaruq597@gmail.com>
2023-11-20 08:27:02 +0700

    Test favoriteBlog function

--- part4/blogapp/utils/list_helper.js
+++ part4/blogapp/utils/list_helper.js
@@ -8,7 +8,17 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, blog) => {
+    if (!favorite || favorite.likes < blog.likes)
+      return { title: blog.title, author: blog.author, likes: blog.likes };
+
+    return favorite;
+  }, null);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
