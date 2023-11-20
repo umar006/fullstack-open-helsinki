be47fee14922703f52496405692bd2e1e4a474cc
umar006 <umaralfaruq597@gmail.com>
2023-11-20 21:22:14 +0700

    Create helper dummy data for testing

--- part4/blogapp/tests/blog_api.test.js
+++ part4/blogapp/tests/blog_api.test.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const helper = require("./test_helper");
 const app = require("../app");
+const Blog = require("../models/blog");
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany();
+
+  for (let blog of helper.initialBlogs) {
+    let newBlog = new Blog(blog);
+    await newBlog.save();
+  }
+});
 
 test("blogs are returned as json", async () => {
   await api
