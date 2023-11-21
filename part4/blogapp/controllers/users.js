a5d126048a569caa02f270c9711a2868efa54614
umar006 <umaralfaruq597@gmail.com>
2023-11-21 15:39:46 +0700

    Make data of user's blogs appear in get users api

--- part4/blogapp/controllers/users.js
+++ part4/blogapp/controllers/users.js
@@ -4,7 +4,11 @@ const User = require("../models/user");
 const userRouter = require("express").Router();
 
 userRouter.get("/", async (request, response) => {
-  const users = await User.find();
+  const users = await User.find().populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+  });
   response.json(users);
 });
 
