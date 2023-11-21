372999274db7591a4c5f08997e0daea9d1c8eb56
umar006 <umaralfaruq597@gmail.com>
2023-11-21 22:40:01 +0700

    Handle error for jwt expired

--- part4/blogapp/utils/middleware.js
+++ part4/blogapp/utils/middleware.js
@@ -5,6 +5,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "JsonWebTokenError") {
     return response.status(401).json({ error: error.message });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({ error: error.message });
   }
 
   next(error);
