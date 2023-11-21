b47ac24e0e75b46344a59dc91cbae96530cf7926
umar006 <umaralfaruq597@gmail.com>
2023-11-21 22:19:17 +0700

    Add error type for JsonWebTokenError

--- part4/blogapp/utils/middleware.js
+++ part4/blogapp/utils/middleware.js
@@ -3,6 +3,10 @@ const logger = require("./logger");
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
+  if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: error.message });
+  }
+
   next(error);
 };
 
