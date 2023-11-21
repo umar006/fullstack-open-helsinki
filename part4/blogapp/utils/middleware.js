ad823391484a1b11ba5d767ddbbd12c4da2ecaa2
umar006 <umaralfaruq597@gmail.com>
2023-11-21 22:40:51 +0700

    Handle error for malformat id

--- part4/blogapp/utils/middleware.js
+++ part4/blogapp/utils/middleware.js
@@ -3,7 +3,9 @@ const logger = require("./logger");
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
-  if (error.name === "JsonWebTokenError") {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "JsonWebTokenError") {
     return response.status(401).json({ error: error.message });
   } else if (error.name === "TokenExpiredError") {
     return response.status(401).json({ error: error.message });
