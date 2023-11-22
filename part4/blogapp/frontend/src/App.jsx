894623e4f68f2c263781a90a4d53e255c9221686
umar006 <umaralfaruq597@gmail.com>
2023-11-22 20:10:08 +0700

    Handle component after login

--- part4/blogapp/frontend/src/App.jsx
+++ part4/blogapp/frontend/src/App.jsx
@@ -4,11 +4,14 @@ import LoginForm from "./components/LoginForm";
 function App() {
   const [user, setUser] = useState(null);
 
-  return (
-    <>
-      <LoginForm setUser={setUser} />
-    </>
-  );
+  if (!user)
+    return (
+      <>
+        <LoginForm setUser={setUser} />
+      </>
+    );
+
+  return <h1>{user.name} logged in</h1>;
 }
 
 export default App;
