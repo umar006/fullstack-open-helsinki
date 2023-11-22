723cf2306d44508ce06d333312768a75bf7290ba
umar006 <umaralfaruq597@gmail.com>
2023-11-22 19:31:12 +0700

    Create login form

--- part4/blogapp/frontend/src/App.jsx
+++ part4/blogapp/frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import LoginForm from "./components/LoginForm";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <LoginForm />
     </>
   );
 }
---
+++ part4/blogapp/frontend/src/components/LoginForm.jsx
@@ -0,0 +1,17 @@
+const LoginForm = () => {
+  return (
+    <>
+      <h1>log in to application</h1>
+      <form>
+        <div>
+          username <input />
+        </div>
+        <div>
+          password <input />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
