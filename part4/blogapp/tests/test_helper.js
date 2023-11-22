b9bad662325aeeba9f5b596e23a12bf519b7816c
umar006 <umaralfaruq597@gmail.com>
2023-11-22 08:46:33 +0700

    Update helper initial blogs and users data

--- part4/blogapp/tests/test_helper.js
+++ part4/blogapp/tests/test_helper.js
@@ -3,35 +3,49 @@ const User = require("../models/user");
 
 const initialBlogs = [
   {
+    _id: "5a422a851b54a676234d17f7",
     title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
+    user: "655c4390b13c51f101d87946",
   },
   {
+    _id: "5a422b3a1b54a676234d17f9",
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     likes: 12,
+    user: "655c4390b13c51f101d87946",
   },
   {
+    _id: "5a422b891b54a676234d17fa",
     title: "First class tests",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
     likes: 10,
+    user: "655c4390b13c51f101d87946",
   },
 ];
 
 const initialUsers = [
   {
+    _id: "655c4390b13c51f101d87946",
     username: "mluukkai",
     name: "Matti Luukkainen",
-    password: "salainen",
+    passwordHash: "salainen",
+    blogs: [
+      "5a422a851b54a676234d17f7",
+      "5a422b3a1b54a676234d17f9",
+      "5a422b891b54a676234d17fa",
+    ],
   },
   {
+    _id: "655cbf1c7625024b0ced4347",
     username: "root",
     name: "Superuser",
-    password: "salainen",
+    passwordHash: "salainen",
+    blogs: [],
   },
 ];
 
