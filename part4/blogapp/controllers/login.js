dceaa861b83cfe8762d4dbe67a0cea2d6e211a55
umar006 <umaralfaruq597@gmail.com>
2023-11-22 08:47:03 +0700

    Await password compare

--- part4/blogapp/controllers/login.js
+++ part4/blogapp/controllers/login.js
@@ -11,7 +11,7 @@ loginRouter.post("/", async (request, response) => {
   const user = await User.findOne({ username });
   const passwordCorrect = !user
     ? null
-    : bcrypt.compare(password, user.passwordHash);
+    : await bcrypt.compare(password, user.passwordHash);
 
   if (!user || !passwordCorrect)
     return response.status(401).json({ error: "invalid username or password" });
