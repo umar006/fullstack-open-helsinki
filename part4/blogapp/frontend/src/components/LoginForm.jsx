ea9b26acbc6e8309cf0cdfb2f5dbddf475dd9e26
umar006 <umaralfaruq597@gmail.com>
2023-11-22 19:48:00 +0700

    Add state for username and password

--- part4/blogapp/frontend/src/components/LoginForm.jsx
+++ part4/blogapp/frontend/src/components/LoginForm.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
+
 const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <>
       <h1>log in to application</h1>
       <form>
         <div>
-          username <input />
+          username <input value={username} onChange={handleUsernameChange} />
         </div>
         <div>
-          password <input />
+          password{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div>
+          <button type="submit">login</button>
         </div>
       </form>
     </>
