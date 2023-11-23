2bc8051b4c7ae6c66c7a034d6c2407df5a4602a3
umar006 <umaralfaruq597@gmail.com>
2023-11-23 23:35:22 +0700

    Sort blog by the number of likes desc

--- part5/blogapp/frontend/src/components/BlogList.jsx
+++ part5/blogapp/frontend/src/components/BlogList.jsx
@@ -64,9 +64,11 @@ const BlogList = () => {
     fetchData();
   }, []);
 
-  const blogList = blogs.map((blog) => (
-    <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />
-  ));
+  const blogList = blogs
+    .sort((blog1, blog2) => blog2.likes - blog1.likes)
+    .map((blog) => (
+      <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />
+    ));
 
   return (
     <>
