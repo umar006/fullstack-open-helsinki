b39b643fbcbc5c798f85d5aff6b823c09c1f6483
umar006 <umaralfaruq597@gmail.com>
2023-11-24 23:17:58 +0700

    Test blog renders the blog's title and author

---
+++ part5/blogapp/frontend/src/components/Blog.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Blog from "./Blog";
+
+test("renders content", () => {
+  const blog = {
+    title: "testTitle",
+    author: "testAuthor",
+    url: "testUrl",
+    user: {},
+  };
+
+  render(<Blog blog={blog} user={{}} />);
+
+  const element = screen.getByText(`${blog.title} ${blog.author}`);
+  expect(element).toBeDefined();
+});
