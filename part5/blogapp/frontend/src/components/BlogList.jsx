d78a8a7213285661e0972cc5c38b43acd8fb878c
umar006 <umaralfaruq597@gmail.com>
2023-11-24 22:07:21 +0700

    Move Blog component to separate file

--- part5/blogapp/frontend/src/components/BlogList.jsx
+++ part5/blogapp/frontend/src/components/BlogList.jsx
@@ -1,61 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 import blogServices from "../services/blogServices";
+import Blog from "./Blog";
 import BlogForm from "./BlogForm";
 import Togglable from "./Togglable";
 import "./BlogList.css";
-
-const Blog = ({ user, blog, blogs, setBlogs }) => {
-  const [likes, setLikes] = useState(blog.likes);
-
-  const handleDeleteBlog = async (event) => {
-    const idToDelete = event.target.id;
-    const confirmDelete = window.confirm(
-      `Delete ${blog.title} by ${blog.author}`,
-    );
-    if (!confirmDelete) return;
-
-    await blogServices.remove(idToDelete);
-
-    setBlogs(blogs.filter((blog) => blog.id !== idToDelete));
-  };
-
-  const handleUpdateLikeBlog = async () => {
-    const idToUpdate = blog.id;
-    const updateLike = likes + 1;
-    const updatedBlog = {
-      ...blog,
-      likes: updateLike,
-    };
-
-    await blogServices.update(idToUpdate, updatedBlog);
-
-    setLikes(updateLike);
-  };
-
-  const isOwnedByUser = user.username === blog.user.username;
-  const userCanDelete = isOwnedByUser ? (
-    <button id={blog.id} onClick={handleDeleteBlog}>
-      {" delete "}
-    </button>
-  ) : null;
-
-  return (
-    <>
-      <div className="blog">
-        {blog.title} {blog.author} {userCanDelete}
-        <Togglable buttonLabelShow="view" buttonLabelHide="hide">
-          <div>
-            <p>{blog.url}</p>
-            <p>
-              {likes} <button onClick={handleUpdateLikeBlog}>like</button>
-            </p>
-            <p>{blog.user.name}</p>{" "}
-          </div>
-        </Togglable>
-      </div>
-    </>
-  );
-};
 
 const BlogList = ({ user }) => {
   const [blogs, setBlogs] = useState([]);
