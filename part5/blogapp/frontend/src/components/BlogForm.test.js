98910ba8f95b6fad74516446d7b9d2037e7b0678
umar006 <umaralfaruq597@gmail.com>
2023-11-26 23:05:26 +0700

    Create test fail BlogForm component

--- part5/blogapp/frontend/src/components/BlogForm.test.js
+++ part5/blogapp/frontend/src/components/BlogForm.test.js
@@ -6,6 +6,10 @@ import blogServices from "../services/blogServices";
 
 jest.mock("../services/blogServices");
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 test("<BlogForm /> success", async () => {
   const mockUseRef = { current: { toggleVisibility: jest.fn() } };
   const component = render(
@@ -30,4 +34,31 @@ test("<BlogForm /> success", async () => {
     { exact: false },
   );
   expect(notifSuccess).toBeInTheDocument();
+});
+
+test("<BlogForm /> success", async () => {
+  const mockUseRef = { current: { toggleVisibility: jest.fn() } };
+  const component = render(<BlogForm blogFormRef={mockUseRef} />);
+
+  blogServices.create.mockRejectedValue({
+    response: { data: { errors: ["something went wrong"] } },
+  });
+
+  const titleField = screen.getByPlaceholderText("title");
+  const authorField = screen.getByPlaceholderText("author");
+  const urlField = screen.getByPlaceholderText("url");
+  const btnCreate = screen.getByText("create");
+
+  await userEvent.type(titleField, "title kucing");
+  await userEvent.type(authorField, "author dinosaurus");
+  await userEvent.type(urlField, "test url");
+
+  await userEvent.click(btnCreate);
+
+  expect(blogServices.create).toHaveBeenCalledTimes(1);
+
+  const errorSuccess = component.getByText("something went wrong", {
+    exact: false,
+  });
+  expect(errorSuccess).toBeInTheDocument();
 });
