302b0fd99d576fd585c1022d626fe5760e255bc4
umar006 <umaralfaruq597@gmail.com>
2023-11-26 17:27:42 +0700

    Create test togglable components before click event

--- part5/blogapp/frontend/src/components/Blog.test.js
+++ part5/blogapp/frontend/src/components/Blog.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
+import Togglable from "./Togglable";
 
 test("display render blog title and author", () => {
   const blog = {
@@ -29,4 +31,21 @@ test("blog url should not rendered", () => {
   const div = container.querySelector(".togglableContent");
 
   expect(div).toHaveStyle("display: none");
+});
+
+describe("Togglable component", () => {
+  let container;
+
+  beforeEach(() => {
+    container = render(
+      <Togglable buttonLabelShow="show" buttonLabelHide="hide">
+        <div>togglable content</div>
+      </Togglable>,
+    ).container;
+  });
+
+  test("at start children should not displayed", () => {
+    const div = container.querySelector(".togglableContent");
+    expect(div).toHaveStyle("display: none");
+  });
 });
