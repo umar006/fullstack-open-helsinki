d37e1c35698099e74f22852eedec2fa8ffea57e4
umar006 <umaralfaruq597@gmail.com>
2023-11-29 20:04:46 +0700

    Create redux store

--- part6/anecdotes/src/main.jsx
+++ part6/anecdotes/src/main.jsx
@@ -2,12 +2,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { legacy_createStore as createStore } from "redux";
 import anecdoteReducer from "./reducer.js";
-import { Provider } from "react-redux";
 
 const store = createStore(anecdoteReducer);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(<App store={store} />);
+};
+
+renderApp();
+store.subscribe(renderApp);
