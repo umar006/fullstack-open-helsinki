0eb21f7fd3995d92bb8ef45584259c770d37a905
umar006 <umaralfaruq597@gmail.com>
2023-11-29 20:41:57 +0700

    Sort anecdotes by highest votes

--- part6/anecdotes/src/App.jsx
+++ part6/anecdotes/src/App.jsx
@@ -3,7 +3,9 @@ import Anecdotes from "./Anecdotes";
 import AnecdoteForm from "./AnecdoteForm";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) =>
+    state.sort((a, b) => b.votes - a.votes),
+  );
 
   return (
     <>
