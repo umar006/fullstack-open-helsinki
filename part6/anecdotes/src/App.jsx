8340cf62763ed2da9ea671a0daa6e8f2c0690794
umar006 <umaralfaruq597@gmail.com>
2023-11-29 20:28:43 +0700

    Refactor anecdotes component

--- part6/anecdotes/src/App.jsx
+++ part6/anecdotes/src/App.jsx
@@ -1,34 +1,5 @@
-import { useDispatch, useSelector } from "react-redux";
-import { giveAnecdoteVote } from "./reducer";
-
-const Anecdote = ({ anecdote }) => {
-  const dispatch = useDispatch();
-
-  const handleGiveVote = (anecdoteId) => {
-    dispatch(giveAnecdoteVote(anecdoteId));
-  };
-
-  return (
-    <div>
-      <span>{anecdote.content}</span>
-      <div>
-        <span>has {anecdote.votes} </span>
-        <button onClick={() => handleGiveVote(anecdote.id)}>vote</button>
-      </div>
-    </div>
-  );
-};
-
-const Anecdotes = ({ anecdotes }) => {
-  return (
-    <>
-      <h1>Anecdotes</h1>
-      {anecdotes.map((anecdote) => (
-        <Anecdote key={anecdote.id} anecdote={anecdote} />
-      ))}
-    </>
-  );
-};
+import { useSelector } from "react-redux";
+import Anecdotes from "./Anecdotes";
 
 const App = () => {
   const anecdotes = useSelector((state) => state);
