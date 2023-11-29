32f1999d9edda7752ad5ff4be8da40db1c8598a8
umar006 <umaralfaruq597@gmail.com>
2023-11-29 19:59:20 +0700

    Forwading redux store

--- part6/anecdotes/src/main.jsx
+++ part6/anecdotes/src/main.jsx
@@ -1,4 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { legacy_createStore as createStore } from "redux";
+import anecdoteReducer from "./reducer.js";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const store = createStore(anecdoteReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
