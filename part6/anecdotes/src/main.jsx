3ab02a8ec8cd70da220d336f40a1526bb598163c
umar006 <umaralfaruq597@gmail.com>
2023-11-30 17:22:51 +0700

    Move the Redux store creation into its own file

--- part6/anecdotes/src/main.jsx
+++ part6/anecdotes/src/main.jsx
@@ -1,16 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import anecdoteReducer from "./reducers/anecdoteReducer";
-import filterReducer from "./reducers/filterReducer";
-
-const store = configureStore({
-  reducer: {
-    anecdotes: anecdoteReducer,
-    filter: filterReducer,
-  },
-});
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
