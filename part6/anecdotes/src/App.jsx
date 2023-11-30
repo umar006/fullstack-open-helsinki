bf25c2240d065dee0cdaa55ef52f733c35430a32
umar006 <umaralfaruq597@gmail.com>
2023-11-30 20:04:24 +0700

    Get anecdotes list from server

--- part6/anecdotes/src/App.jsx
+++ part6/anecdotes/src/App.jsx
@@ -1,9 +1,21 @@
-import Anecdotes from "./components/Anecdotes";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import AnecdoteForm from "./components/AnecdoteForm";
+import Anecdotes from "./components/Anecdotes";
 import AnecdotesFilter from "./components/AnecdotesFilter";
 import Notification from "./components/Notification";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+import anecdoteServices from "./services/anecdoteServices";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteServices
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  });
+
   return (
     <>
       <h1>Anecdotes</h1>
