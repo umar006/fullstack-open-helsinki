5355d5020ca9c73da3f74e6c812276c92dbeaebe
umar006 <umaralfaruq597@gmail.com>
2023-11-30 19:52:55 +0700

    Set anecdote reducer initial state to empty array

--- part6/anecdotes/src/reducers/anecdoteReducer.js
+++ part6/anecdotes/src/reducers/anecdoteReducer.js
@@ -1,25 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  { id: 1, content: "If it hurts, do it more often.", votes: 0 },
-  {
-    id: 2,
-    content: "Adding manpower to a late software project makes it later!",
-    votes: 0,
-  },
-  {
-    id: 3,
-    content:
-      "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    votes: 0,
-  },
-];
-
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const anecdoteSlice = createSlice({
   name: "anecdote",
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const content = action.payload;
