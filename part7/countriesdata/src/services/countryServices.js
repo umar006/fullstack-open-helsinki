6358b59236430a2f017b6b7a83aad825a31d5364
umar006 <umaralfaruq597@gmail.com>
2023-12-03 21:09:49 +0700

    Change from promise to async/await

--- part7/countriesdata/src/services/countryServices.js
+++ part7/countriesdata/src/services/countryServices.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
 
-const getAll = () => {
-  const req = axios.get(baseUrl + "/all");
-  return req.then((res) => res.data);
+const getAll = async () => {
+  const response = await axios.get(baseUrl + "/all");
+
+  return response.data;
 };
 
 export default { getAll };
--- part7/countriesdata/src/services/openWeatherServices.js
+++ part7/countriesdata/src/services/openWeatherServices.js
@@ -3,12 +3,11 @@ import axios from "axios";
 const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 const baseUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric`;
 
-const get = (latlng) => {
+const get = async (latlng) => {
   const [lat, lon] = latlng;
-  const req = axios.get(baseUrl + `&lat=${lat}&lon=${lon}`);
-  return req.then((res) => {
-    return res.data;
-  });
+  const res = await axios.get(baseUrl + `&lat=${lat}&lon=${lon}`);
+
+  return res.data;
 };
 
 export default { get };
