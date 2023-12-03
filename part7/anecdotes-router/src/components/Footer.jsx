8f88536f7c2ea52440be972a17da0e6c62922ce5
umar006 <umaralfaruq597@gmail.com>
2023-12-03 12:38:58 +0700

    Add style padding top to footer

--- part7/anecdotes-router/src/components/Footer.jsx
+++ part7/anecdotes-router/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer>
+  <footer style={{ paddingTop: 16 }}>
     Anecdote app for <a href="https://fullstackopen.com/">Full Stack Open</a>.
     See{" "}
     <a href="https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js">
