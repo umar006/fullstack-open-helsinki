7c09c4d85bc7f017543320f33f072e445a6771a5
umar006 <umaralfaruq597@gmail.com>
2023-12-04 07:53:39 +0700

    Remove unused service

--- part7/countriesdata/src/services/countryServices.js
+++ part7/countriesdata/src/services/countryServices.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
 
-const getAll = async () => {
-  const response = await axios.get(baseUrl + "/all");
+const getOne = async (countryName) => {
+  const response = await axios.get(baseUrl + "/name/" + countryName);
 
   return response.data;
 };
 
-export default { getAll };
+export default { getOne };
