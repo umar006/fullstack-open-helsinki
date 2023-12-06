61d3654b1ffc413bfd0460a4f2d5bdf428ccca7a
umar006 <umaralfaruq597@gmail.com>
2023-12-06 21:58:01 +0700

    Create blog slice reducers

---
+++ part7/blogapp-redux/frontend/src/reducers/blogReducer.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const blogSlice = createSlice({
+  name: "blogs",
+  initialState: [],
+  reducers: {},
+});
+
+export const {} = blogSlice.actions;
+
+export default blogSlice.reducer;
