b67fdd90e925a9c7ead86bf6985673c950c0beda
umar006 <umaralfaruq597@gmail.com>
2023-12-06 22:34:53 +0700

    Use blogs reducer to display blog list

--- part7/blogapp-redux/frontend/src/components/BlogList.jsx
+++ part7/blogapp-redux/frontend/src/components/BlogList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setBlogs } from "../reducers/blogReducer";
 import blogServices from "../services/blogServices";
 import Blog from "./Blog";
 import BlogForm from "./BlogForm";
@@ -8,30 +9,33 @@ import Notification from "./Notification";
 import Togglable from "./Togglable";
 
 const BlogList = ({ user }) => {
-  const [blogs, setBlogs] = useState([]);
+  const [oldBlogs, setOldBlogs] = useState([]);
   const blogFormRef = useRef();
+  const blogs = useSelector((state) => state.blogs);
   const notification = useSelector((state) => state.notification);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await blogServices.getAll();
-      setBlogs(response);
+      dispatch(setBlogs(response));
+      setOldBlogs(response);
     };
 
     fetchData();
   }, []);
 
   const blogList = blogs
-    .sort((blog1, blog2) => blog2.likes - blog1.likes)
     .map((blog) => (
       <Blog
         key={blog.id}
         user={user}
         blog={blog}
         blogs={blogs}
-        setBlogs={setBlogs}
+        setBlogs={setOldBlogs}
       />
-    ));
+    ))
+    .sort((blog1, blog2) => blog2.likes - blog1.likes);
 
   return (
     <>
@@ -41,7 +45,11 @@ const BlogList = ({ user }) => {
         buttonLabelShow="new blog"
         buttonLabelHide="cancel"
       >
-        <BlogForm blogs={blogs} setBlogs={setBlogs} blogFormRef={blogFormRef} />
+        <BlogForm
+          blogs={blogs}
+          setBlogs={setOldBlogs}
+          blogFormRef={blogFormRef}
+        />
       </Togglable>
       {blogList}
     </>
