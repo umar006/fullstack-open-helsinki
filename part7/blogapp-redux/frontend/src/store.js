2aa762356abad82b799afacdb829b36091c4ef70
umar006 <umaralfaruq597@gmail.com>
2023-12-06 21:58:08 +0700

    Add blog slice reducers to store

--- part7/blogapp-redux/frontend/src/store.js
+++ part7/blogapp-redux/frontend/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import blogReducer from "./reducers/blogReducer";
 import notificationReducer from "./reducers/notificationReducer";
 
 export default configureStore({
   reducer: {
+    blogs: blogReducer,
     notification: notificationReducer,
   },
 });
