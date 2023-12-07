ce06e7c7d3ece9a677748361caafc0e2a4268238
umar006 <umaralfaruq597@gmail.com>
2023-12-07 12:24:26 +0700

    Add login slice reducers to store

--- part7/blogapp-redux/frontend/src/store.js
+++ part7/blogapp-redux/frontend/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import blogReducer from "./reducers/blogReducer";
+import loginReducer from "./reducers/loginReducer";
 import notificationReducer from "./reducers/notificationReducer";
 
 export default configureStore({
   reducer: {
     blogs: blogReducer,
+    login: loginReducer,
     notification: notificationReducer,
   },
 });
