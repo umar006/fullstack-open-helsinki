f0f0f2f7f6a02e97494bfa92370934cb1c60e050
umar006 <umaralfaruq597@gmail.com>
2023-12-07 12:23:58 +0700

    Create login redux state slice

---
+++ part7/blogapp-redux/frontend/src/reducers/loginReducer.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const loginSlice = createSlice({
+  name: "login",
+  initialState: null,
+  reducers: {},
+});
+
+export const {} = loginSlice.actions;
+
+export default loginSlice.reducer;
