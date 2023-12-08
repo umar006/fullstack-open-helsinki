84c36d5699aaa234a3346bb2ce505a87c7700d43
umar006 <umaralfaruq597@gmail.com>
2023-12-08 17:29:59 +0700

    Create login reducer

---
+++ part7/blogapp/frontend-react-query/src/reducers/loginReducer.js
@@ -0,0 +1,12 @@
+const loginReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return action.payload;
+    case "LOGOUT":
+      return null;
+    default:
+      return state;
+  }
+};
+
+export default loginReducer;
