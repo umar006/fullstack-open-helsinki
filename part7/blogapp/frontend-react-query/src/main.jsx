0261b5996e23c276e258816ada2847a8e551f747
umar006 <umaralfaruq597@gmail.com>
2023-12-08 17:29:35 +0700

    Provide login context to all components

--- part7/blogapp/frontend-react-query/src/main.jsx
+++ part7/blogapp/frontend-react-query/src/main.jsx
@@ -1,15 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import { LoginContextProvider } from "./contexts/LoginContext.jsx";
 import { NotificationContextProvider } from "./contexts/NotificationContext.jsx";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <NotificationContextProvider>
-      <App />
-    </NotificationContextProvider>
+    <LoginContextProvider>
+      <NotificationContextProvider>
+        <App />
+      </NotificationContextProvider>
+    </LoginContextProvider>
   </QueryClientProvider>,
 );
