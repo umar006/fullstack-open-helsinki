cb7e8f2528a4241af926c141cd4d48541326b195
umar006 <umaralfaruq597@gmail.com>
2023-12-08 14:55:43 +0700

    Provide notification context to all components

--- part7/blogapp/frontend-react-query/src/main.jsx
+++ part7/blogapp/frontend-react-query/src/main.jsx
@@ -2,11 +2,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { NotificationContextProvider } from "./contexts/NotificationContext.jsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <NotificationContextProvider>
+      <App />
+    </NotificationContextProvider>
   </QueryClientProvider>,
 );
