e16fa005c5c538697095a7eadbe713d0e4262b83
umar006 <umaralfaruq597@gmail.com>
2023-12-11 14:58:08 +0700

    Provide react router to App component

--- part7/blogapp/frontend-react-query/src/main.jsx
+++ part7/blogapp/frontend-react-query/src/main.jsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import { LoginContextProvider } from "./contexts/LoginContext.jsx";
 import { NotificationContextProvider } from "./contexts/NotificationContext.jsx";
@@ -9,10 +10,12 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <LoginContextProvider>
-      <NotificationContextProvider>
-        <App />
-      </NotificationContextProvider>
-    </LoginContextProvider>
+    <Router>
+      <LoginContextProvider>
+        <NotificationContextProvider>
+          <App />
+        </NotificationContextProvider>
+      </LoginContextProvider>
+    </Router>
   </QueryClientProvider>,
 );
