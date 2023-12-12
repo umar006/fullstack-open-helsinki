cbf413ac6dd44e3661f594fe99dad2a42661c9d7
umar006 <umaralfaruq597@gmail.com>
2023-12-12 21:43:17 +0700

    Add style to login form

--- part7/blogapp/frontend-react-query/src/components/LoginForm.jsx
+++ part7/blogapp/frontend-react-query/src/components/LoginForm.jsx
@@ -52,8 +52,8 @@ const LoginForm = () => {
   };
 
   return (
-    <>
-      <h1>log in to application</h1>
+    <div className="px-8 py-8">
+      <h1 className="text-3xl font-bold mb-8">log in to application</h1>
       <Notification message={notif} />
       <form onSubmit={handleLogin}>
         <div>
@@ -62,6 +62,12 @@ const LoginForm = () => {
             id="username"
             value={username}
             onChange={handleUsernameChange}
+            required
+            className="mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "
           />
         </div>
         <div>
@@ -71,15 +77,25 @@ const LoginForm = () => {
             type="password"
             value={password}
             onChange={handlePasswordChange}
+            required
+            className="mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "
           />
         </div>
         <div>
-          <button type="submit" id="btn-login">
+          <button
+            type="submit"
+            id="btn-login"
+            className="px-4 py-2 bg-sky-500 mt-8 text-slate-100 font-medium"
+          >
             login
           </button>
         </div>
       </form>
-    </>
+    </div>
   );
 };
 
