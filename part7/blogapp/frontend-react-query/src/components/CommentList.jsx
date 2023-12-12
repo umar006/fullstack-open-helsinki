c29f155d6aeb4c5a45c6ee23511121d5dab2ace8
umar006 <umaralfaruq597@gmail.com>
2023-12-12 08:52:19 +0700

    Add form to add new comment

--- part7/blogapp/frontend-react-query/src/components/CommentList.jsx
+++ part7/blogapp/frontend-react-query/src/components/CommentList.jsx
@@ -1,9 +1,35 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import blogServices from "../services/blogServices";
 
 const CommentList = () => {
   const blogId = useParams().id;
+  const [comment, setComment] = useState("");
+
+  const queryClient = useQueryClient();
+
+  const newCommentMutation = useMutation({
+    mutationFn: (newComment) => blogServices.createComment(blogId, newComment),
+    onSuccess: (newComment) => {
+      const comments = queryClient.getQueryData(["blogs/comments", blogId]);
+      queryClient.setQueryData(
+        ["blogs/comments", blogId],
+        comments.concat(newComment),
+      );
+
+      setComment("");
+    },
+    onError: (err) => {
+      console.error(err);
+    },
+  });
+
+  const handleCreateComment = (e) => {
+    e.preventDefault();
+
+    newCommentMutation.mutate({ comment });
+  };
 
   const result = useQuery({
     queryKey: ["blogs/comments", blogId],
@@ -25,6 +51,11 @@ const CommentList = () => {
   return (
     <div>
       <h3>Comments</h3>
+      <form onSubmit={handleCreateComment}>
+        comment{" "}
+        <input value={comment} onChange={(e) => setComment(e.target.value)} />{" "}
+        <button type="submit">create</button>
+      </form>
       <ul>{commentList}</ul>
     </div>
   );
