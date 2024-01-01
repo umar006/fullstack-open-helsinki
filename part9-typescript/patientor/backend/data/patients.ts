ce4afcbec7328f9fea673f246d1c6fbd0daaefcf
umar006 <umaralfaruq597@gmail.com>
2024-01-01 07:49:52 +0700

    Create patient data types

--- part9-typescript/patientor/backend/data/patients.ts
+++ part9-typescript/patientor/backend/data/patients.ts
@@ -1,4 +1,6 @@
-const patientsData = [
+import { Patient } from "../src/types";
+
+const patientsData: Patient[] = [
   {
     id: "d2773336-f723-11e9-8f0b-362b9e155667",
     name: "John McClane",
--- part9-typescript/patientor/backend/src/types.ts
+++ part9-typescript/patientor/backend/src/types.ts
@@ -3,3 +3,14 @@ export interface DiagnosisData {
   name: string;
   latin?: string;
 }
+
+export type Gender = "male" | "female" | "other";
+
+export interface Patient {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: Gender;
+  occupation: string;
+}
