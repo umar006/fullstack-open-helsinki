b13948f96fc775e29d9e5cb330d73c30f0bda1f2
umar006 <umaralfaruq597@gmail.com>
2024-01-01 14:57:18 +0700

    Create NewPatient type

--- part9-typescript/patientor/backend/src/types.ts
+++ part9-typescript/patientor/backend/src/types.ts
@@ -8,6 +8,8 @@ export type Gender = "male" | "female" | "other";
 
 export type NonSensitivePatient = Omit<Patient, "ssn">;
 
+export type NewPatient = Omit<Patient, "id">;
+
 export interface Patient {
   id: string;
   name: string;
