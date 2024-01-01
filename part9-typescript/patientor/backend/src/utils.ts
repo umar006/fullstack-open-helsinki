ce12c361ff13f846cac64da975eae18dd17a4450
umar006 <umaralfaruq597@gmail.com>
2024-01-01 15:59:17 +0700

    Validate request body

--- part9-typescript/patientor/backend/src/utils.ts
+++ part9-typescript/patientor/backend/src/utils.ts
@@ -1,6 +1,19 @@
 import { NewPatient } from "./types";
 
 export const toNewPatient = (data: unknown): NewPatient => {
+  if (!data || typeof data !== "object") {
+    throw new Error("Incorrect or missing data");
+  }
+
+  const nameExist = "name" in data;
+  const dateOfBirthExist = "dateOfBirth" in data;
+  const genderExist = "gender" in data;
+  const ssnExist = "ssn" in data;
+  const occupationExist = "occupation" in data;
+  if (!nameExist || !dateOfBirthExist || !genderExist || !ssnExist || !occupationExist) {
+    throw new Error("Incorrect data: some fields are missing");
+  }
+
   const newPatient: NewPatient = {
     name: data.name,
     dateOfBirth: data.dateOfBirth,
