472b8c8d3f15f20facfed6cff0c54018627ab31d
umar006 <umaralfaruq597@gmail.com>
2024-01-02 18:59:36 +0700

    Change course parts data

--- part9-typescript/courseapp/src/App.tsx
+++ part9-typescript/courseapp/src/App.tsx
@@ -6,19 +6,36 @@ function App() {
   const courseName = "Half Stack application development";
   const courseParts = [
     {
-      id: 1,
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
     },
     {
-      id: 2,
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
     },
     {
-      id: 3,
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
+    },
+    {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial:
+        "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
     },
   ];
 
