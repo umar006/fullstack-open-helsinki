957b48aaff4b0bcb1a685e3a2d1821207e035bf6
umar006 <umaralfaruq597@gmail.com>
2024-01-03 18:57:41 +0700

    Fetch flight diaries list

--- part9-typescript/flight-diary/frontend/src/App.tsx
+++ part9-typescript/flight-diary/frontend/src/App.tsx
@@ -1,4 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Diary } from "./types";
+
 function App() {
+  const [diaries, setDiaries] = useState<Diary[]>([]);
+
+  useEffect(() => {
+    const fetchDiaries = async () => {
+      const response = await axios.get<Diary[]>(
+        "http://localhost:3000/api/diaries",
+      );
+      const diariesList = response.data;
+      setDiaries(diariesList);
+    };
+
+    fetchDiaries().catch((e) => console.error(e));
+  }, []);
+
+  console.log(diaries);
+
   return <h1>Hello bro</h1>;
 }
 
