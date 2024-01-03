89056869ff7feb0681cb036244e2f4107afbab44
umar006 <umaralfaruq597@gmail.com>
2024-01-03 18:57:18 +0700

    Create Diary type

---
+++ part9-typescript/flight-diary/frontend/src/types.ts
@@ -0,0 +1,26 @@
+const WEATHER = {
+  Sunny: "sunny",
+  Rainy: "rainy",
+  Cloudy: "cloudy",
+  Stormy: "stormy",
+  Windy: "windy",
+} as const;
+
+type Weather = (typeof WEATHER)[keyof typeof WEATHER];
+
+const VISIBILITY = {
+  Great: "great",
+  Good: "good",
+  Ok: "ok",
+  Poor: "poor",
+} as const;
+
+type Visibility = (typeof VISIBILITY)[keyof typeof VISIBILITY];
+
+export interface Diary {
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment: string;
+}
