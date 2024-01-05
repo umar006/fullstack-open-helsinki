61860925934983107035fe972a7f52dda2cb81ac
umar006 <umaralfaruq597@gmail.com>
2024-01-05 16:17:05 +0700

    Add new property entires to Patient

--- part9-typescript/patientor/backend/src/types.ts
+++ part9-typescript/patientor/backend/src/types.ts
@@ -14,9 +14,12 @@ type GenderKey = keyof typeof GENDER;
 
 export type Gender = (typeof GENDER)[GenderKey];
 
-export type NonSensitivePatient = Omit<Patient, "ssn">;
+export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
 
 export type NewPatient = Omit<Patient, "id">;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Entry {}
 
 export interface Patient {
   id: string;
@@ -25,4 +28,5 @@ export interface Patient {
   ssn: string;
   gender: Gender;
   occupation: string;
+  entries?: Entry[];
 }
