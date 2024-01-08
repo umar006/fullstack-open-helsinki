9382357825fdfe60028694e9f7a00098467e2e77
umar006 <umaralfaruq597@gmail.com>
2024-01-08 12:31:35 +0700

    Add diagnoses description by diagnosis codes

--- part9-typescript/patientor/frontend/src/components/PatientDetailPage/PatientEntryList.tsx
+++ part9-typescript/patientor/frontend/src/components/PatientDetailPage/PatientEntryList.tsx
@@ -1,20 +1,47 @@
-import { Patient } from "../../types";
+import { useEffect, useState } from "react";
+import diagnosisService from "../../services/diagnoses";
+import { Diagnosis, Patient } from "../../types";
 
 interface Props {
   patient: Patient;
 }
 
 const PatientEntryList = ({ patient }: Props) => {
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
+
+  useEffect(() => {
+    const fetchDiagnoses = async () => {
+      const diagnoses = await diagnosisService.getAll();
+      setDiagnoses(diagnoses);
+    };
+
+    fetchDiagnoses().catch((error) => console.error(error));
+  }, []);
+
+  if (!diagnoses.length) {
+    return <div>No Diagnoses</div>;
+  }
+
+  const getDiagnosisByCode = (code: string[]) => {
+    const filteredDiagnoses = diagnoses.filter((diagnosis) =>
+      code.includes(diagnosis.code),
+    );
+
+    return filteredDiagnoses.map((diagnosis) => (
+      <li key={diagnosis.code}>
+        {diagnosis.code} -&gt; {diagnosis.name}
+      </li>
+    ));
+  };
+
   const entryList = patient.entries.map((entry) => (
     <div key={entry.id}>
       <p>
         {entry.date} | {entry.description}
       </p>
-      {entry.diagnosisCodes?.map((dc, idx) => (
-        <ul key={idx}>
-          <li>{dc}</li>
-        </ul>
-      ))}
+      <ul>
+        {entry.diagnosisCodes && getDiagnosisByCode(entry.diagnosisCodes)}
+      </ul>
     </div>
   ));
 
