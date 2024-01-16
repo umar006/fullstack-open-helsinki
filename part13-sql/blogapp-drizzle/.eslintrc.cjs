752965b3fa03c9612a39a87ff3c5818ed34340cb
umar006 <umaralfaruq597@gmail.com>
2024-01-16 22:39:59 +0700

    Initial project commit

---
+++ part13-sql/blogapp-drizzle/.eslintrc.cjs
@@ -0,0 +1,16 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
