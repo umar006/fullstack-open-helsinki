91d1a62a10335929cd1acbcfe5dad511635fd4f8
umar006 <umaralfaruq597@gmail.com>
2024-01-21 05:59:38 +0700

    Initial setup fastify

---
+++ part13-sql/blogapp-drizzle/src/index.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const server = fastify();
+
+server.listen({ port: 3001 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
