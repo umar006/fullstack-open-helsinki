547219e69b27aa9935e671b298ae37aaa6648f07
umar006 <umaralfaruq597@gmail.com>
2024-01-21 06:00:31 +0700

    Create api to get all blogs

--- part13-sql/blogapp-drizzle/src/index.ts
+++ part13-sql/blogapp-drizzle/src/index.ts
@@ -1,6 +1,18 @@
 import fastify from "fastify";
+import db from "../db/index.js";
+import { blog } from "./schema/blogs.js";
 
 const server = fastify();
+
+server.get("/api/blogs", async (req, reply) => {
+  try {
+    const blogs = await db.select().from(blog);
+
+    return blogs;
+  } catch (e) {
+    console.error(e);
+  }
+});
 
 server.listen({ port: 3001 }, (err, address) => {
   if (err) {
